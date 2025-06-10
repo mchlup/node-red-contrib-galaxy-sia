09fa519d80e56357c040528e67db73eaa7266ad6
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-10 07:27:38 +0200

    Update galaxy-sia-config.js

--- galaxy-sia-config.js
+++ galaxy-sia-config.js
@@ -1,18 +1,27 @@
+// Node-RED konfigurační node pro Galaxy SIA DC-09
 module.exports = function(RED) {
   function GalaxySIAConfigNode(n) {
     RED.nodes.createNode(this, n);
-    this.account = n.account;
-    this.panelIP = n.panelIP;
+
+    // Základní parametry
+    this.account = n.account || "";
+    this.panelIP = n.panelIP || "";
     this.panelPort = Number(n.panelPort) || 10002;
-    this.siaLevel = Number(n.siaLevel);
-    this.encryption = n.encryption === "true";
+    this.siaLevel = Number(n.siaLevel) || 4;
+
+    // Šifrování
+    // Podporuje jak string, tak HEX (pro AES-128)
+    this.encryption = n.encryption === true || n.encryption === "true";
     this.encryptionKey = n.encryptionKey || "";
-    this.encryptionHex = n.encryptionHex === "true";
-    this.connectOnDemand = n.connectOnDemand === "true";
-    this.heartbeatInterval = Number(n.heartbeatInterval);
-    this.periodicReportInterval = Number(n.periodicReportInterval);
-    this.discardTestMessages = n.discardTestMessages === "true";
+    this.encryptionHex = n.encryptionHex === true || n.encryptionHex === "true";
+
+    // Další volby
+    this.connectOnDemand = n.connectOnDemand === true || n.connectOnDemand === "true";
+    this.heartbeatInterval = Number(n.heartbeatInterval) || 60;
+    this.periodicReportInterval = Number(n.periodicReportInterval) || 0;
+    this.discardTestMessages = n.discardTestMessages === true || n.discardTestMessages === "true";
     this.deviceList = n.deviceList || "";
   }
+
   RED.nodes.registerType("galaxy-sia-config", GalaxySIAConfigNode);
 };
