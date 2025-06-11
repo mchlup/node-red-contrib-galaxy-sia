7bd56b789f26d0e933553ccf1f3a93d46eda7777
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-11 13:57:13 +0200

    Update galaxy-sia-config.js

--- galaxy-sia-config.js
+++ galaxy-sia-config.js
@@ -1,10 +1,9 @@
-/**
- * Galaxy SIA Config Node
- */
 module.exports = function(RED) {
   function GalaxySiaConfigNode(config) {
     RED.nodes.createNode(this, config);
 
+    // --- Všechny vlastnosti, které HTML form očekává: ---
+    this.name                   = config.name;
     this.panelIP                = config.panelIP;
     this.panelPort              = Number(config.panelPort) || 10002;
     this.account                = config.account;
@@ -13,17 +12,17 @@ module.exports = function(RED) {
     this.encryptionKey          = config.encryptionKey || "";
     this.encryptionHex          = !!config.encryptionHex;
     this.discardTestMessages    = !!config.discardTestMessages;
-    this.ackType                = config.ackType || "A_CRLF";
+    this.ackType                = config.ackType;
     this.ackCustom              = config.ackCustom || "";
     this.periodicReportInterval = Number(config.periodicReportInterval) || 0;
 
-    // PIN uložený v credentials
+    // PIN je uloženo jako credentials, necháme ho tam:
     this.pin = this.credentials.pin;
   }
 
-  RED.nodes.registerType("galaxy-sia-config", GalaxySiaConfigNode, {
-    credentials: {
-      pin: { type: "password" }
-    }
-  });
+  RED.nodes.registerType(
+    "galaxy-sia-config",
+    GalaxySiaConfigNode,
+    { credentials: { pin: { type: "password" } } }
+  );
 };
