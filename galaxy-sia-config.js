df1166688e6f5e6f80da5b87d8b08c9919402c1a
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-11 14:27:51 +0200

    Update galaxy-sia-config.js

--- galaxy-sia-config.js
+++ galaxy-sia-config.js
@@ -2,7 +2,7 @@ module.exports = function(RED) {
   function GalaxySiaConfigNode(config) {
     RED.nodes.createNode(this, config);
 
-    // --- Všechny vlastnosti, které HTML form očekává: ---
+    // Všechny vlastnosti musí přesně odpovídat HTML (a flow):
     this.name                   = config.name;
     this.panelIP                = config.panelIP;
     this.panelPort              = Number(config.panelPort) || 10002;
@@ -12,11 +12,9 @@ module.exports = function(RED) {
     this.encryptionKey          = config.encryptionKey || "";
     this.encryptionHex          = !!config.encryptionHex;
     this.discardTestMessages    = !!config.discardTestMessages;
-    this.ackType                = config.ackType;
+    this.ackType                = config.ackType || "A_CRLF";
     this.ackCustom              = config.ackCustom || "";
     this.periodicReportInterval = Number(config.periodicReportInterval) || 0;
-
-    // PIN je uloženo jako credentials, necháme ho tam:
     this.pin = this.credentials.pin;
   }
 
