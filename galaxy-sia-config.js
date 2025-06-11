be43be226a8376ce9b9290ec96a3930c38023422
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-11 20:50:55 +0200

    Update galaxy-sia-config.js

--- galaxy-sia-config.js
+++ galaxy-sia-config.js
@@ -5,21 +5,26 @@ module.exports = function(RED) {
   function GalaxySiaConfigNode(config) {
     RED.nodes.createNode(this, config);
 
-    this.name                   = config.name;
-    this.panelIP                = config.panelIP;
-    this.panelPort              = Number(config.panelPort) || 10002;
-    this.account                = config.account;
-    this.siaLevel               = Number(config.siaLevel) || 4;
-    this.encryption             = !!config.encryption;
-    this.encryptionKey          = config.encryptionKey || "";
-    this.encryptionHex          = !!config.encryptionHex;
-    this.discardTestMessages    = !!config.discardTestMessages;
-    this.ackType                = config.ackType || "A_CRLF";
-    this.ackCustom              = config.ackCustom || "";
+    this.name = config.name;
+    this.panelIP = config.panelIP;
+    this.panelPort = Number(config.panelPort) || 10002;
+    this.account = config.account;
+    this.siaLevel = Number(config.siaLevel) || 4;
+    this.encryption = !!config.encryption;
+    this.encryptionKey = config.encryptionKey || "";
+    this.encryptionHex = !!config.encryptionHex;
+    this.discardTestMessages = !!config.discardTestMessages;
+    this.ackType = config.ackType || "A_CRLF";
+    this.ackCustom = config.ackCustom || "";
     this.periodicReportInterval = Number(config.periodicReportInterval) || 0;
-
     this.pin = this.credentials.pin;
-  }
+    
+    // Add status object
+    this.status = {};
+    
+    // Initialize the socket property
+    this.socket = null;
+}
 
   RED.nodes.registerType(
     "galaxy-sia-config",
