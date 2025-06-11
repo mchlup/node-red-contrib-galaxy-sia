ffadcdbd2d50a3d155f9faa1231d2780b5a46c75
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-11 07:56:23 +0200

    Create galaxy-sia-disarm.js

---
+++ galaxy-sia-disarm.js
@@ -0,0 +1,26 @@
+module.exports = function(RED) {
+  const siaCmd = require('../lib/sia-command');
+
+  function DisarmNode(config) {
+    RED.nodes.createNode(this, config);
+    const cfg = RED.nodes.getNode(config.config);
+    const node = this;
+
+    this.partition = config.partition;
+    this.on('input', () => {
+      const cmd = siaCmd.disarm(cfg.account, this.partition, cfg.pin);
+      cfg.socket.write(cmd, 'ascii');
+      node.log(`Disarming partition ${this.partition}`);
+      node.send({ payload: 'disarmed' });
+    });
+
+    this.on('close', done => done());
+  }
+
+  RED.nodes.registerType('galaxy-sia-disarm', DisarmNode, {
+    defaults: {
+      config: { type: 'galaxy-sia-config', required: true },
+      partition: { value: '1' }
+    }
+  });
+};
