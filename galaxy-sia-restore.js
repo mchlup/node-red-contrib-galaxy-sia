99ba7fb253602d758955c8b9e1bb1376b6b51071
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-11 07:56:59 +0200

    Create galaxy-sia-restore.js

---
+++ galaxy-sia-restore.js
@@ -0,0 +1,26 @@
+module.exports = function(RED) {
+  const siaCmd = require('../lib/sia-command');
+
+  function RestoreNode(config) {
+    RED.nodes.createNode(this, config);
+    const cfg = RED.nodes.getNode(config.config);
+    const node = this;
+    this.zone = config.zone;
+
+    this.on('input', () => {
+      const cmd = siaCmd.restore(cfg.account, this.zone);
+      cfg.socket.write(cmd, 'ascii');
+      node.log(`Restoring zone ${this.zone}`);
+      node.send({ payload: `restored ${this.zone}` });
+    });
+
+    this.on('close', done => done());
+  }
+
+  RED.nodes.registerType('galaxy-sia-restore', RestoreNode, {
+    defaults: {
+      config: { type: 'galaxy-sia-config', required: true },
+      zone: { value: '01' }
+    }
+  });
+};
