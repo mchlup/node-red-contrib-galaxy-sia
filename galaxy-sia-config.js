e428e2ae9e3a9907152f7f688d9edc0dcb8dccef
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-11 21:41:16 +0200

    Update galaxy-sia-config.js

--- galaxy-sia-config.js
+++ galaxy-sia-config.js
@@ -11,6 +11,11 @@ module.exports = function(RED) {
     this.panelPort = Number(n.panelPort) || 10002;
     this.siaLevel = Number(n.siaLevel) || 4;
 
+    // Mapování entit (parse JSON, pokud je validní)
+    try { this.zoneMap = n.zoneMap ? JSON.parse(n.zoneMap) : {}; } catch(e) { this.zoneMap = {}; }
+    try { this.userMap = n.userMap ? JSON.parse(n.userMap) : {}; } catch(e) { this.userMap = {}; }
+    try { this.areaMap = n.areaMap ? JSON.parse(n.areaMap) : {}; } catch(e) { this.areaMap = {}; }
+
     // Šifrování
     this.encryption = n.encryption === true || n.encryption === "true";
     this.encryptionKey = n.encryptionKey || "";
@@ -27,9 +32,8 @@ module.exports = function(RED) {
     this.ackType = n.ackType || "ECHO_TRIM_END";
     this.ackCustom = n.ackCustom || "";
 
-    // Nově: debug flag
+    // Debug flag
     this.debug = n.debug === true || n.debug === "true";
   }
-
   RED.nodes.registerType("galaxy-sia-config", GalaxySIAConfigNode);
 };
