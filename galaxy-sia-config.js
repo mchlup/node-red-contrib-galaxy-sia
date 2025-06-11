072bc6899496d85e776a469e6ed5f625b7892e20
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-11 13:37:45 +0200

    Update galaxy-sia-config.js

--- galaxy-sia-config.js
+++ galaxy-sia-config.js
@@ -1,29 +1,29 @@
 /**
- * Galaxy SIA Config Node with auto-reconnect/back-off
+ * Galaxy SIA Config Node
  */
 module.exports = function(RED) {
   function GalaxySiaConfigNode(config) {
     RED.nodes.createNode(this, config);
 
-    // základní nastavení
-    this.panelIP               = config.panelIP;
-    this.panelPort             = Number(config.panelPort) || 10002;
-    this.account               = config.account;
-    this.siaLevel              = Number(config.siaLevel) || 4;
-    this.encryption            = !!config.encryption;
-    this.encryptionKey         = config.encryptionKey || "";
-    this.encryptionHex         = !!config.encryptionHex;
-    this.discardTestMessages   = !!config.discardTestMessages;
-    this.ackType               = config.ackType || "A_CRLF";
-    this.ackCustom             = config.ackCustom || "";
-    // nová vlastnost
+    this.panelIP                = config.panelIP;
+    this.panelPort              = Number(config.panelPort) || 10002;
+    this.account                = config.account;
+    this.siaLevel               = Number(config.siaLevel) || 4;
+    this.encryption             = !!config.encryption;
+    this.encryptionKey          = config.encryptionKey || "";
+    this.encryptionHex          = !!config.encryptionHex;
+    this.discardTestMessages    = !!config.discardTestMessages;
+    this.ackType                = config.ackType || "A_CRLF";
+    this.ackCustom              = config.ackCustom || "";
     this.periodicReportInterval = Number(config.periodicReportInterval) || 0;
 
-    // PIN v credentials
+    // PIN uložený v credentials
     this.pin = this.credentials.pin;
   }
 
   RED.nodes.registerType("galaxy-sia-config", GalaxySiaConfigNode, {
-    credentials: { pin: { type: "password" } }
+    credentials: {
+      pin: { type: "password" }
+    }
   });
 };
