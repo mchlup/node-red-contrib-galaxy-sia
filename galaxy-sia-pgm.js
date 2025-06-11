e1ecb68701b3b75b745fc9f9da120387a51a824e
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-11 07:57:16 +0200

    Create galaxy-sia-pgm.js

---
+++ galaxy-sia-pgm.js
@@ -0,0 +1,29 @@
+module.exports = function(RED) {
+  const siaCmd = require('../lib/sia-command');
+
+  function PgmNode(config) {
+    RED.nodes.createNode(this, config);
+    const cfg = RED.nodes.getNode(config.config);
+    const node = this;
+    this.output = config.output;
+    this.action = config.action; // 'on' or 'off'
+
+    this.on('input', () => {
+      const actCode = this.action === 'on' ? '01' : '00';
+      const cmd = siaCmd.pgm(cfg.account, this.output, actCode);
+      cfg.socket.write(cmd, 'ascii');
+      node.log(`PGM ${this.action} output ${this.output}`);
+      node.send({ payload: `pgm ${this.action} ${this.output}` });
+    });
+
+    this.on('close', done => done());
+  }
+
+  RED.nodes.registerType('galaxy-sia-pgm', PgmNode, {
+    defaults: {
+      config: { type: 'galaxy-sia-config', required: true },
+      output: { value: '1' },
+      action: { value: 'on' }
+    }
+  });
+};
