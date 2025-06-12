02a8ad01fe801313386560a5881b79ed8806f642
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-12 08:07:34 +0200

    Update galaxy-sia-bypass.js

--- galaxy-sia-bypass.js
+++ galaxy-sia-bypass.js
@@ -17,10 +17,19 @@ module.exports = function(RED) {
     this.on('close', done => done());
   }
 
-  RED.nodes.registerType('galaxy-sia-bypass', BypassNode, {
+  RED.nodes.registerType("galaxy-sia-bypass", BypassNode, {
+    category: "Galaxy SIA Connector",  // Změna kategorie
     defaults: {
-      config: { type: 'galaxy-sia-config', required: true },
-      zone: { value: '01' }
-    }
+      name: { value: "" },
+      config: { type: "galaxy-sia-config", required: true }
+    },
+    color: "#a6d7a8",
+    inputs: 1,
+    outputs: 1,
+    icon: "font-awesome/fa-shield",
+    label: function() {
+      return this.name || "galaxy-sia-bypass";
+    },
+    paletteLabel: "bypass"
   });
 };
