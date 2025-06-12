474b08b2b2b6cc137f9b6b939563c5a0b290a5c5
mchlup <132652169+mchlup@users.noreply.github.com>
2025-06-12 07:47:05 +0200

    Update galaxy-sia-config.js

--- galaxy-sia-config.js
+++ galaxy-sia-config.js
@@ -1,32 +1,37 @@
 module.exports = function(RED) {
     function GalaxySiaConfigNode(config) {
         RED.nodes.createNode(this, config);
-        this.name = config.name;
+        
+        // Store all configuration values
+        this.name = config.name || "";
         this.panelIP = config.panelIP;
         this.panelPort = config.panelPort;
         this.account = config.account;
-        this.siaLevel = config.siaLevel;
-        this.encryption = config.encryption;
+        this.siaLevel = config.siaLevel || 4;
+        this.encryption = config.encryption || false;
         this.encryptionKey = config.encryptionKey;
-        this.encryptionHex = config.encryptionHex;
-        this.discardTestMessages = config.discardTestMessages;
-        this.ackType = config.ackType;
+        this.encryptionHex = config.encryptionHex || false;
+        this.discardTestMessages = config.discardTestMessages || false;
+        this.ackType = config.ackType || "A_CRLF";
         this.ackCustom = config.ackCustom;
-        this.periodicReportInterval = config.periodicReportInterval;
+        this.periodicReportInterval = config.periodicReportInterval || 0;
         this.userId = config.userId;
         this.userCode = config.userCode;
-        this.heartbeatInterval = config.heartbeatInterval;
+        this.heartbeatInterval = config.heartbeatInterval || 60;
         this.zoneMap = config.zoneMap;
         this.userMap = config.userMap;
         this.areaMap = config.areaMap;
         this.externalMappingPath = config.externalMappingPath;
-        if (config.credentials) {
-            this.credentials = config.credentials;
+
+        // Properly handle credentials
+        if (this.credentials) {
+            this.pin = this.credentials.pin;
         }
     }
+
     RED.nodes.registerType("galaxy-sia-config", GalaxySiaConfigNode, {
         credentials: {
-            pin: {type:"password"}
+            pin: {type: "password"}
         }
     });
-};
+}
